7cec450447d4e6f37cc24945888559f5dd5890b2
kmothapo023a <kmothapo023a@student.wethinkcode.co.za>
2023-12-12 12:53:03 +0200

    added rounds to game

--- src/main/java/org/koketjo/NumberGame.java
+++ src/main/java/org/koketjo/NumberGame.java
@@ -5,7 +5,10 @@ import java.util.Random;
 import java.util.Scanner;
 
 public class NumberGame {
+    private static int correctCount = 0;
+    private static int rounds = 1;
     private static final Random random = new Random();
+    private static Scanner scan = new Scanner(System.in);
     public static int generateNumber()
     {
         return random.nextInt(100) + 1;
@@ -17,40 +20,104 @@ public class NumberGame {
 
     public static int getGuess()
     {
-        Scanner scan = new Scanner(System.in);
-        System.out.println("Enter a number between 1 and 100: ");
+        System.out.println("Guess a number between 1 and 100: ");
 
         return scan.nextInt();
     }
 
-    public static void checkGuess(int guess, int random)
+    public static boolean checkGuess(int guess, int random)
     {
         if(guess == random)
         {
             System.out.println("Correct guess, the number is: " + guess);
-        } else
+            correctCount();
+            return true;
+
+        } else if(guess < random)
         {
-            System.out.println("Incorrect "+guess + " is not the number.");
+            System.out.println("Incorrect, "+guess + " is too low.");
+            return false;
+
+        } else {
+
+            System.out.println("Incorrect, "+guess + " is too high.");
+            return false;
+        }
+
+    }
+
+    public static void correctCount()
+    {
+        correctCount++;
+    }
+
+    public static void displayScore()
+    {
+        System.out.println("-----------------------------");
+        System.out.println("|                           |");
+        System.out.println("|    YOUR SCORE : " + correctCount +"         |");
+        System.out.println("|                           |");
+        System.out.println("----------------------------");
+        System.out.println("        GAME OVER!          ");
+    }
+
+    public static void playGame()
+    {
+        int number = 0;
+        int random = generateNumber();
+        int guesses = getNumberOfGuesses();
+        int guessesInitial = guesses;
+
+        while(number < guessesInitial)
+        {
+            System.out.println("You have " + guesses + " attempts!");
+            int guess = getGuess();
+            if(checkGuess(guess,random))
+            {
+                anotherRound();
+                break;
+            }
+            guesses--;
+            number++;
+        }
+
+        if(number == guessesInitial)
+        {
+            System.out.println("You have run out of guesses!");
+            anotherRound();
+
+        }
+    }
+
+    public static void anotherRound()
+    {
+        System.out.println("Enter \"p\" to play another round or enter \"q\" to quit game.");
+        String input = scan.next();
+
+        if(input.equalsIgnoreCase("q"))
+        {
+            displayScore();
+
+        } else if(input.equalsIgnoreCase("p"))
+        {
+            if(rounds == 3)
+            {
+                System.out.println("You have played a full 3 rounds");
+                displayScore();
+
+            } else
+            {
+                System.out.println("Entered another round! The maximum number of rounds is 3.");
+                rounds++;
+                System.out.println("You are on round " + rounds + "!");
+                playGame();
+            }
         }
     }
 
     public static void main(String[] args) {
         System.out.println("Welcome to the Number Game!");
+        playGame();
 
-        int number = 0;
-        int random = generateNumber();
-        int guesses = getNumberOfGuesses();
-
-        while(number < guesses)
-        {
-            int guess = getGuess();
-            checkGuess(guess,random);
-            number++;
-        }
-
-        if(number == guesses)
-        {
-            System.out.println("You have run out of guesses!");
-        }
     }
 }
