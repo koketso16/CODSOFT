73483724d206525c0ff62bbb2d852241e4887cf1
kmothapo023a <kmothapo023a@student.wethinkcode.co.za>
2023-12-15 00:00:21 +0200

    working on GUI for number game

---
+++ src/main/NumberGame/java/org/koketjo/NumberGameGUI.java
@@ -0,0 +1,153 @@
+package org.koketjo;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.Random;
+
+public class NumberGameGUI extends JFrame {
+    private static int correctCount = 0;
+    private static int rounds = 1;
+    private static final Random random = new Random();
+    private static int numberOfGuesses = getNumberOfGuesses();
+    private static int randomNumber = generateNumber();
+    private final JLabel infoLabel;
+    private final JTextField guessTextField;
+
+    public NumberGameGUI() {
+
+        setTitle("Number Game");
+        setSize(400, 200);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        setLayout(new BorderLayout());
+
+        infoLabel = new JLabel("Welcome to the Number Game!");
+        infoLabel.setFont(new Font("Arial", Font.PLAIN, 18));
+        add(infoLabel, BorderLayout.NORTH);
+
+        guessTextField = new JTextField();
+        add(guessTextField, BorderLayout.CENTER);
+
+        JButton guessButton = new JButton("Guess");
+        JButton quitButton = new JButton("Quit");
+
+        JPanel buttonPanel = new JPanel();
+        buttonPanel.add(guessButton);
+        buttonPanel.add(quitButton);
+        add(buttonPanel, BorderLayout.SOUTH);
+
+        guessButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                handleGuess();
+            }
+        });
+
+        quitButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                quitGame();
+            }
+        });
+    }
+
+    private void handleGuess() {
+        String guessText = guessTextField.getText();
+        try {
+            int guess = Integer.parseInt(guessText);
+
+            if (numberOfGuesses == 0) {
+                displayOutOfGuessesMessage();
+            }
+
+            if (guess == randomNumber) {
+                JOptionPane.showMessageDialog(this, "Correct guess, the number is: " + guess);
+                correctCount++;
+                playGame();
+            } else if (guess < randomNumber) {
+                JOptionPane.showMessageDialog(this, "Incorrect, " + guess + " is too low.\nYou have " + numberOfGuesses + " attempts left!");
+            } else {
+                JOptionPane.showMessageDialog(this, "Incorrect, " + guess + " is too high.\nYou have " + numberOfGuesses + " attempts left!");
+            }
+        } catch (NumberFormatException ex) {
+            JOptionPane.showMessageDialog(this, "Please enter a valid integer.\nYou have " + numberOfGuesses + " attempts left!");
+        }
+        numberOfGuesses--;
+    }
+
+    private void displayOutOfGuessesMessage() {
+        JButton quitButton = new JButton("Quit");
+        JButton anotherRoundButton = new JButton("Another round");
+
+        quitButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                quitGame();
+            }
+        });
+
+        anotherRoundButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+                Window window = SwingUtilities.getWindowAncestor((Component) e.getSource());
+                window.dispose();
+
+                if (rounds < 3) {
+
+                    playGame();
+
+                    numberOfGuesses = getNumberOfGuesses();
+                    randomNumber = generateNumber();
+                    rounds++;
+                    infoLabel.setText("Guess a number between 1 and 100:");
+                    guessTextField.setText("");
+                } else {
+                    JOptionPane.showMessageDialog(NumberGameGUI.this, "You have played a full 3 rounds.");
+                    quitGame();
+                }
+            }
+        });
+
+        Object[] options = {quitButton, anotherRoundButton};
+        JOptionPane.showOptionDialog(this, "You have run out of guesses!", "Game Over",
+                JOptionPane.YES_NO_OPTION, JOptionPane.INFORMATION_MESSAGE, null, options, options[0]);
+        guessTextField.setText("");
+    }
+
+
+    private void quitGame() {
+        displayScore();
+        System.exit(0);
+    }
+
+    private static int generateNumber() {
+        return random.nextInt(100) + 1;
+    }
+
+    public static int getNumberOfGuesses()
+    {
+        return random.nextInt(6) + 5;
+    }
+
+    private void playGame() {
+        infoLabel.setText("Guess a number between 1 and 100:");
+        guessTextField.setText("");
+
+    }
+
+    private void displayScore() {
+        JOptionPane.showMessageDialog(this, "Your final score: " + correctCount, "Game Over", JOptionPane.INFORMATION_MESSAGE);
+    }
+
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(() -> {
+            NumberGameGUI gameGUI = new NumberGameGUI();
+            gameGUI.playGame();
+            gameGUI.setLocationRelativeTo(null);
+            gameGUI.setVisible(true);
+        });
+    }
+}
