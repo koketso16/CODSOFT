6b9b28a858d08eb20b639d8a3d5388c38233e13a
koketso16 <99620457+koketso16@users.noreply.github.com>
2024-01-06 01:27:15 +0200

    Currency converter and Student management system
    I had to do them separately so I could set up Javafx for the GUI

---
+++ CODSOFT(JavaFx)/src/main/CurrencyConverter/java/org/koketjo/FetchFromAPI.java
@@ -0,0 +1,27 @@
+package org.koketjo;
+
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+import kong.unirest.HttpResponse;
+import kong.unirest.JsonNode;
+import kong.unirest.Unirest;
+import java.math.BigDecimal;
+
+public class FetchFromAPI {
+
+    private static final String API_KEY = "73c118ff499f61268bca3a02";
+    public static double fetchExchangeRate(String baseCurrency, String targetCurrency) {
+        String apiUrl = "https://open.er-api.com/v6/latest/" + baseCurrency;
+        HttpResponse<JsonNode> response = Unirest.get(apiUrl)
+                .header("Accept", "application/json")
+                .queryString("apikey", API_KEY)
+                .asJson();
+
+        JsonObject rates = JsonParser.parseString(response.getBody().toString()).getAsJsonObject().getAsJsonObject("rates");
+        BigDecimal exchangeRate = rates.getAsJsonPrimitive(targetCurrency).getAsBigDecimal();
+
+//        System.out.println("Exchange Rate: 1 " + baseCurrency + " = " + exchangeRate + " " + targetCurrency);
+
+        return Double.parseDouble(String.valueOf(exchangeRate));
+    }
+}
